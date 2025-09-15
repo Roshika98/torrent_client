933ca8c320e7fea90734967ca9b96797c7b67961
Roshika Perera <pereraroshika98@gmail.com>
2025-09-15 22:41:16 +0530

    Refactor writeMany function to improve file handling and stream management

--- learning/streams/writemany.js
+++ learning/streams/writemany.js
@@ -25,15 +25,50 @@ const filehandler2 = require("fs");
 // 	console.timeEnd("writeMany2");
 // })();
 
+// (async () => {
+// 	console.time("writeMany3");
+// 	const stream = filehandler2.createWriteStream("test3.txt", {
+// 		flags: "w",
+// 	});
+// 	for (let i = 0; i < 1000000; i++) {
+// 		const buff = Buffer.from(`a\n`, "utf-8");
+// 		stream.write(buff);
+// 	}
+// 	console.timeEnd("writeMany3");
+// 	stream.end(() => {});
+// })();
+
 (async () => {
-	console.time("writeMany3");
-	const stream = filehandler2.createWriteStream("test3.txt", {
-		flags: "w",
-	});
+	console.time("writeMany4");
+
+	const filehandle = await filehandler.open("test4.txt", "w+");
+
+	const stream = filehandle.createWriteStream();
+	// console.log(stream.writableHighWaterMark);
+
+	// stream.on("drain", () => {
+	// 	console.log("Drain event");
+	// });
+
+	// const buff = Buffer.alloc(16383, 10);
+
+	// stream.write(buff);
+	// stream.write(Buffer.alloc(1, 'a'));
+	// console.log(stream.writableLength);
+	
+
+	
+
 	for (let i = 0; i < 1000000; i++) {
 		const buff = Buffer.from(`a\n`, "utf-8");
+
 		stream.write(buff);
 	}
-	console.timeEnd("writeMany3");
-	stream.end(() => {});
+
+	stream.on("finish", () => {
+		console.timeEnd("writeMany4");
+		filehandle.close();
+	})
+
+	// filehandle.close();
 })();
