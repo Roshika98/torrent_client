a5e9b7caa579cd7ebf54b827f819b10105d5f38c
Roshika Perera <pereraroshika98@gmail.com>
2025-09-21 23:59:24 +0530

    Implement custom ReadStream class for file reading using streams

---
+++ learning/streams/customReadable/customReadable.js
@@ -0,0 +1,32 @@
+const { Readable } = require("stream");
+const fs = require("fs");
+
+class ReadStream extends Readable {
+	constructor({ highWaterMark, fileName }) {
+		super({ highWaterMark });
+		this.fileName = fileName;
+		this.fd = null;
+		this.position = 0;
+	}
+
+	_construct(callback) {
+		fs.open(this.fileName, "r", (err, fd) => {
+			if (err) {
+				return callback(err);
+			}
+			this.fd = fd;
+			callback();
+		});
+	}
+
+	_read(size) {
+		const buffer = Buffer.alloc(size);
+		fs.read(this.fd, buffer, 0, size, null, (err, bytesRead) => {
+			if (err) {
+				this.destroy(err);
+				return;
+			}
+			this.push(bytesRead > 0 ? buffer.subarray(0, bytesRead) : null);
+		});
+	}
+}
